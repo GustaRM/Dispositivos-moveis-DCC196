b23c0d3389f6c83a6e5fa81985569812d464f664
Gustavo Ribeiro Martins <gustavoribeirom2001@gmail.com>
2023-06-06 23:38:00 -0300

    Update Telas do Menu do Cliente

---
+++ app/src/main/java/com/example/ibarba/Cortes.java
@@ -0,0 +1,14 @@
+package com.example.ibarba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cortes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cortes);
+    }
+}
---
+++ app/src/main/java/com/example/ibarba/Meus_Horarios.java
@@ -0,0 +1,14 @@
+package com.example.ibarba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meus_Horarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_horarios);
+    }
+}
