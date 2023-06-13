927a04fc3344cb10a23352c8e1d35d901c9a3987
Kelmer-EK <135773426+Kelmer-EK@users.noreply.github.com>
2023-06-13 13:18:07 -0300

    Tela de cadastro de clientes

---
+++ app/src/main/java/com/example/ibarba/Cadastro_cliente.java
@@ -0,0 +1,65 @@
+package com.example.ibarba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Cadastro_cliente extends AppCompatActivity {
+
+    private EditText txtNome;
+    private EditText txtTelefone;
+    private EditText txtEmail;
+    private EditText txtSenha;
+    private EditText txtConfirmaSenha;
+    private Button btnConcluir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_cliente);
+
+
+        // Inicializar os componentes
+        txtNome = findViewById(R.id.txtNome);
+        txtTelefone = findViewById(R.id.txtTelefone);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+        txtConfirmaSenha = findViewById(R.id.txtConfirmaSenha);
+        btnConcluir = findViewById(R.id.btnConcluir);
+
+        // Configurar o clique do botão "Concluir"
+        btnConcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obter os valores dos campos
+                String nome = txtNome.getText().toString();
+                String telefone = txtTelefone.getText().toString();
+                String email = txtEmail.getText().toString();
+                String senha = txtSenha.getText().toString();
+                String confirmaSenha = txtConfirmaSenha.getText().toString();
+
+                // Verificar se a senha e a confirmação são iguais
+                if (senha.equals(confirmaSenha)) {
+                    // Criar um novo usuário com os dados fornecidos
+                    Usuario novoUsuario = new Usuario(nome, telefone, email, "cliente", senha);
+
+                    // Adicionar o novo usuário à lista existente na MainActivity
+                    MainActivity.adicionarUsuario(novoUsuario);
+
+                    // Exibir mensagem de sucesso
+                    Toast.makeText(Cadastro_cliente.this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+
+                    // Encerrar a tela de cadastro e voltar para a MainActivity
+                    finish();
+                } else {
+                    // Senha e confirmação não correspondem, exibir mensagem de erro
+                    Toast.makeText(Cadastro_cliente.this, "A senha e a confirmação não correspondem.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/ibarba/MainActivity.java
+++ app/src/main/java/com/example/ibarba/MainActivity.java
@@ -16,7 +16,15 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     // Cria uma lista para armazenar os usuários
-    private List<Usuario> usuarios;
+    private static List<Usuario> usuarios = new ArrayList<>();
+
+    public static List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public static void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +58,15 @@ public class MainActivity extends AppCompatActivity {
 
                 if (usuarioEncontrado != null) {
                     // Usuário válido, redirecione para a tela apropriada com base no tipo de usuário
-                    if (usuarioEncontrado.getTipoUsuario().equals("1")) {
+                    if (usuarioEncontrado.getTipoUsuario().equals("cliente")) {
                         // Redirecionar para a tela "Bem vindo cliente"
                         Intent intent = new Intent(MainActivity.this, Menu_cliente.class);
                         startActivity(intent);
-                    } else if (usuarioEncontrado.getTipoUsuario().equals("2")) {
+                    } else if (usuarioEncontrado.getTipoUsuario().equals("profissional")) {
                         // Redirecionar para a tela "Bem vindo profissional"
                         Intent intent = new Intent(MainActivity.this, Menu_gestor.class);
                         startActivity(intent);
-                    } else if (usuarioEncontrado.getTipoUsuario().equals("3")) {
+                    } else if (usuarioEncontrado.getTipoUsuario().equals("gestor")) {
                         // Redirecionar para a tela "Bem vindo gestor"
                         Intent intent = new Intent(MainActivity.this, Menu_gestor.class);
                         startActivity(intent);
@@ -76,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Redirecionar para a tela de cadastro
-                Intent intent = new Intent(MainActivity.this, Menu_gestor.class);
+                Intent intent = new Intent(MainActivity.this, Cadastro_cliente.class);
                 startActivity(intent);
             }
         });
@@ -84,25 +92,25 @@ public class MainActivity extends AppCompatActivity {
     }
     // Método para popular a lista de usuários com exemplos
     private void popularUsuarios() {
-        usuarios.add(new Usuario("João", "123456789", "joao@exemplo.com", "1", "senha1"));
-        usuarios.add(new Usuario("Maria", "987654321", "maria@exemplo.com", "1", "senha2"));
-        usuarios.add(new Usuario("Pedro", "456789123", "pedro@exemplo.com", "1", "senha3"));
-        usuarios.add(new Usuario("Ana", "321654987", "ana@exemplo.com", "1", "senha4"));
-        usuarios.add(new Usuario("Carlos", "654987321", "carlos@exemplo.com", "1", "senha5"));
-        usuarios.add(new Usuario("Mariana", "789123456", "mariana@exemplo.com", "1", "senha6"));
-        usuarios.add(new Usuario("Rafael", "987321654", "rafael@exemplo.com", "1", "senha7"));
-        usuarios.add(new Usuario("Camila", "789456123", "camila@exemplo.com", "1", "senha8"));
-        usuarios.add(new Usuario("Lucas", "321789654", "lucas@exemplo.com", "1", "senha9"));
-        usuarios.add(new Usuario("Fernanda", "456123789", "fernanda@exemplo.com", "1", "senha10"));
-        usuarios.add(new Usuario("Guilherme", "852963741", "guilherme@exemplo.com", "2", "senha11"));
-        usuarios.add(new Usuario("Patrícia", "369852147", "patricia@exemplo.com", "2", "senha12"));
-        usuarios.add(new Usuario("André", "147852369", "andre@exemplo.com", "2", "senha13"));
-        usuarios.add(new Usuario("Júlia", "963741852", "julia@exemplo.com", "2", "senha14"));
-        usuarios.add(new Usuario("Roberto", "258963147", "roberto@exemplo.com", "2", "senha15"));
-        usuarios.add(new Usuario("Carolina", "741852963", "carolina@exemplo.com", "2", "senha16"));
-        usuarios.add(new Usuario("Renato", "369147852", "renato@exemplo.com", "3", "senha17"));
-        usuarios.add(new Usuario("Amanda", "852147369", "amanda@exemplo.com", "1", "senha18"));
-        usuarios.add(new Usuario("Thiago", "147369852", "thiago@exemplo.com", "1", "senha19"));
-        usuarios.add(new Usuario("Gabriela", "741963852", "gabriela@exemplo.com", "1", "senha20"));
+        usuarios.add(new Usuario("João", "123456789", "joao@exemplo.com", "cliente", "senha1"));
+        usuarios.add(new Usuario("Maria", "987654321", "maria@exemplo.com", "cliente", "senha2"));
+        usuarios.add(new Usuario("Pedro", "456789123", "pedro@exemplo.com", "cliente", "senha3"));
+        usuarios.add(new Usuario("Ana", "321654987", "ana@exemplo.com", "cliente", "senha4"));
+        usuarios.add(new Usuario("Carlos", "654987321", "carlos@exemplo.com", "cliente", "senha5"));
+        usuarios.add(new Usuario("Mariana", "789123456", "mariana@exemplo.com", "cliente", "senha6"));
+        usuarios.add(new Usuario("Rafael", "987321654", "rafael@exemplo.com", "cliente", "senha7"));
+        usuarios.add(new Usuario("Camila", "789456123", "camila@exemplo.com", "cliente", "senha8"));
+        usuarios.add(new Usuario("Lucas", "321789654", "lucas@exemplo.com", "cliente", "senha9"));
+        usuarios.add(new Usuario("Fernanda", "456123789", "fernanda@exemplo.com", "cliente", "senha10"));
+        usuarios.add(new Usuario("Guilherme", "852963741", "guilherme@exemplo.com", "profissional", "senha11"));
+        usuarios.add(new Usuario("Patrícia", "369852147", "patricia@exemplo.com", "profissional", "senha12"));
+        usuarios.add(new Usuario("André", "147852369", "andre@exemplo.com", "profissional", "senha13"));
+        usuarios.add(new Usuario("Júlia", "963741852", "julia@exemplo.com", "profissional", "senha14"));
+        usuarios.add(new Usuario("Roberto", "258963147", "roberto@exemplo.com", "profissional", "senha15"));
+        usuarios.add(new Usuario("Carolina", "741852963", "carolina@exemplo.com", "profissional", "senha16"));
+        usuarios.add(new Usuario("Renato", "369147852", "renato@exemplo.com", "gestor", "senha17"));
+        usuarios.add(new Usuario("Amanda", "852147369", "amanda@exemplo.com", "cliente", "senha18"));
+        usuarios.add(new Usuario("Thiago", "147369852", "thiago@exemplo.com", "cliente", "senha19"));
+        usuarios.add(new Usuario("Gabriela", "741963852", "gabriela@exemplo.com", "cliente", "senha20"));
     }
 }
