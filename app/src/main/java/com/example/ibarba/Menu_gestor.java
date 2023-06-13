cd566cb78ec9673428667bfdc500c4ccd5d74965
Kelmer-EK <135773426+Kelmer-EK@users.noreply.github.com>
2023-06-13 11:30:11 -0300

    Menu gestor, lista de usuários, direcionamento de login conforme o tipo de usuário

---
+++ app/src/main/java/com/example/ibarba/Menu_gestor.java
@@ -0,0 +1,14 @@
+package com.example.ibarba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_gestor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_gestor);
+    }
+}
---
+++ app/src/main/java/com/example/ibarba/Usuario.java
@@ -0,0 +1,59 @@
+package com.example.ibarba;
+
+public class Usuario {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String tipoUsuario;
+    private String senha;
+
+    // Construtor
+    public Usuario(String nome, String telefone, String email, String tipoUsuario, String senha) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.tipoUsuario = tipoUsuario;
+        this.senha = senha;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
