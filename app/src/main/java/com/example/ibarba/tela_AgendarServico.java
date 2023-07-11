6cf6d7e60bc0dd2e0681e3dd40b1acb63eac753f
Kelmer-EK <135773426+Kelmer-EK@users.noreply.github.com>
2023-07-11 11:23:47 -0300

    Tela agendar serviço funcionando.

--- app/src/main/java/com/example/ibarba/tela_AgendarServico.java
+++ app/src/main/java/com/example/ibarba/tela_AgendarServico.java
@@ -9,42 +9,143 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class tela_AgendarServico extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class tela_AgendarServico extends AppCompatActivity {
+
+    private Spinner spinnerServico;
+    private Spinner spinnerProfissional;
+    private Spinner spinnerData;
+    private Spinner spinnerHorario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar_servico);
 
-        Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.servicos, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        spinnerServico = findViewById(R.id.spinnerServico);
+        spinnerProfissional = findViewById(R.id.spinnerProfissional);
+        spinnerData = findViewById(R.id.spinnerData);
+        spinnerHorario = findViewById(R.id.spinnerHorario);
 
-        Spinner spinner2 = findViewById(R.id.spinner2);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.horarios, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter2);
-        spinner2.setOnItemSelectedListener(this);
+        configuraServicos();
+        configuraProfissionais();
 
-        Spinner spinner3 = findViewById(R.id.spinner3);
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.profisssionais, android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner3.setAdapter(adapter3);
-        spinner3.setOnItemSelectedListener(this);
+        ArrayAdapter<String> usuarioAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, obterListaProfissionais());
+        spinnerProfissional.setAdapter(usuarioAdapter);
+
+        spinnerServico.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+         //       Toast.makeText(tela_AgendarServico.this,parent.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(tela_AgendarServico.this,"Nenhum selecionado",Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        spinnerProfissional.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            //    Toast.makeText(tela_AgendarServico.this,parent.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+                configuraDatas();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(tela_AgendarServico.this,"Nenhum selecionado",Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        spinnerData.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+         //       Toast.makeText(tela_AgendarServico.this,parent.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+                configuraHorarios();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(tela_AgendarServico.this,"Nenhum selecionado",Toast.LENGTH_LONG).show();
+            }
+
+        });
+        spinnerHorario.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+       //         Toast.makeText(tela_AgendarServico.this,parent.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(tela_AgendarServico.this,"Nenhum selecionado",Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString();
-        Toast.makeText(adapterView.getContext(), "", Toast.LENGTH_SHORT).show();
+    private void configuraServicos() {
+        ArrayAdapter<String> servicoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, obterListaServicos());
+        spinnerServico.setAdapter(servicoAdapter);
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
+    private void configuraProfissionais() {
+        ArrayAdapter<String> usuarioAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, obterListaProfissionais());
+        spinnerProfissional.setAdapter(usuarioAdapter);
     }
 
+    private void configuraDatas() {
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, obterListaDatas());
+        spinnerData.setAdapter(dataAdapter);
+    }
 
+    private void configuraHorarios() {
+        ArrayAdapter<String> horarioAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, obterListaHorarios());
+        spinnerHorario.setAdapter(horarioAdapter);
+    }
+
+    private List<String> obterListaServicos() {
+        List<String> listaServicos = new ArrayList<>();
+        listaServicos.add("Serviço 1");
+        listaServicos.add("Serviço 2");
+        listaServicos.add("Serviço 3");
+        return listaServicos;
+    }
+
+    private List<String> obterListaProfissionais() {
+        List<String> listaProfissionais = new ArrayList<>();
+        listaProfissionais.add("Fulano");
+        listaProfissionais.add("Beltrano");
+        listaProfissionais.add("Cicrano");
+        return listaProfissionais;
+    }
+
+    private List<String> obterListaDatas() {
+        List<String> listaDatas = new ArrayList<>();
+        listaDatas.add("11/07/2023");
+        listaDatas.add("12/07/2023");
+        listaDatas.add("13/07/2023");
+        return listaDatas;
+    }
+    private List<String> obterListaHorarios() {
+        List<String> listaHorarios = new ArrayList<>();
+        listaHorarios.add("08:00");
+        listaHorarios.add("09:00");
+        listaHorarios.add("10:00");
+        return listaHorarios;
+    }
 }
